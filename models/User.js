6cf7184a6ab4d5e28ee31fa61d90a0fa00e61f38
urmee04 <urmee04sust@gmail.com>
2025-08-27 10:44:06 -0400

    add instance method to check password

--- models/User.js
+++ models/User.js
@@ -34,6 +34,11 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+//instance method to compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 //create User model from schema
 const User = mongoose.model("User", userSchema);
 
